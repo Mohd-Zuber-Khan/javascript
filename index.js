57f4b63f4ffe571f0edec480d5177ecc9d7e6187
zuber <kzuberkhan0786@gmail.com>
2023-02-15 22:55:01 +0530

    first javascript

---
+++ index.js
@@ -0,0 +1,128 @@
+// var x=4;
+// var y=7;
+
+// var z=x+y;
+// document.write(z);
+// console.log(z);
+// alert(z);
+
+
+// document.write("<br>");
+
+// var p=3;
+// var q= 4;
+// var r=p-q;
+// document.write(r);
+// console.log(r);
+// alert(r);
+
+
+// document.write("<br>")
+
+// var a= 5;
+// var b = 9;
+// var c= a*b;
+// document.write(c);
+// console.log(c);
+// alert(c);
+
+// document.write("</br>");
+
+// var l=20;
+// var m= 10;
+// var n= l/m;
+// console.log(n);
+// document.write(n);
+// alert(n);
+
+// var x= 3.14;
+// var r = 5;
+
+// var a = x*r*r;
+// document.write(a);
+// console.log(a);
+
+// document.write("</br>");
+
+// var pie= 3.14;
+// var r= 5;
+// var Area= pie*r*r;
+// document.write("area of circle is =" ,Area);
+
+// var l = 10;
+//  var b= 30;
+//  var p = 2*(l+b);
+// var area =l*b;
+// console.log(area);
+// document.write("Area of rectangle =" ,area);
+
+// document.write("</br>");
+
+//  console.log(2*(l+b));
+//  document.write("parameter of rectangle = " ,p);
+
+//  document.write("</br>");
+
+// var breath= 6;
+// var height= 8;
+// var areaTriangle = ( breath*height)/2;
+// document.write("area of triangle =" ,areaTriangle);
+// console.log( areaTriangle);
+
+// document.write("</br>")
+
+// var side = 3;
+// var squareArea = side*side;
+// document.write("Square Area =" ,squareArea);
+
+
+let side= 3;
+
+let areaOfSquare = side*side;
+document.write(areaOfSquare);
+
+document.write("</br>");
+
+let length =4;
+let breath = 6;
+ let areaOfRectangle= length*breath;
+
+ document.write(areaOfRectangle);
+
+ document.write("</br>");
+
+
+
+ let base = 7;
+ let height = 10;
+
+ let areaOfParellogram = base*height;
+ document.write(areaOfParellogram);
+
+ document.write("</br>");
+ 
+
+ let radius = 40;
+ let pie = 3.14;
+
+ areaOfCircle = pie*radius*radius;
+
+ document.write(areaOfCircle);
+
+document.write("</br>");
+
+ length = 2;
+ breath = 4;
+ perimeterOfRectangle = 2*(length + breath);
+
+ document.write(perimeterOfRectangle);
+
+ document.write("</br>");
+ 
+
+ let p = 10;
+ let q= 4;
+ 
+var age=prompt("whatisyourage?");
+document.write(age);
+
